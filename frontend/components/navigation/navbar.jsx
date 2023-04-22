8c7913963b984031c765694b392861c95850b281
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-04-22 18:16:04 +0100

    Added and Restyle navbar for better appearance

--- frontend/components/navigation/navbar.jsx
+++ frontend/components/navigation/navbar.jsx
@@ -1,12 +1,32 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import styles from '../../styles/Navbar.module.css';
-export default function Navbar() {
+
+const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
-			<a href='#' target={'_blank'}>
-				<img className={styles.logo} src='/404.gif'></img>
-			</a>
-			<ConnectButton></ConnectButton>
+			<div className={styles.logo}>
+				<a href='/home'>Logo</a>
+			</div>
+			<div className={styles.searchBar}>
+				<input type='text' placeholder='Search' />
+
+				<ul className={styles.menu}>
+					<li>
+						<a href='/home'>Home</a>
+					</li>
+					<li>
+						<a href='/market'>Market</a>
+					</li>
+					<li>
+						<a href='/create'>Create</a>
+					</li>
+				</ul>
+			</div>
+			<div className={styles.buttons}>
+				<ConnectButton />
+			</div>
 		</nav>
 	);
-}
+};
+
+export default Navbar;
